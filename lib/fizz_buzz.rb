e932412cd5fedb285aae1af46a9ea9583f70a1ef
keitakn <keita.koga.work@gmail.com>
2018-03-27 21:43:16 +0900

    modify #3 elsifを消して早期returnするようにリファクタリング

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,12 +4,16 @@
 # @return [String]
 def fizz_buzz(n)
   if n % 15 == 0
-    "Fizz Buzz"
-  elsif n % 3 == 0
-    "Fizz"
-  elsif n % 5 == 0
-    "Buzz"
-  else
-    n.to_s
+    return "Fizz Buzz"
   end
+
+  if n % 3 == 0
+    return "Fizz"
+  end
+
+  if n % 5 == 0
+    return "Buzz"
+  end
+
+  n.to_s
 end
