b79fda8ee2c9c778530bf48ec58477378eb3f52e
keitakn <keita.koga.work@gmail.com>
2018-03-27 21:38:13 +0900

    add #3 fizz_buzz用テストクラスを追加

---
+++ test/fizz_buzz_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "./lib/fizz_buzz"
+
+# fizz_buzzメソッドのテストクラス
+class FizzBuzzTest < Minitest::Test
+  # fizz_buzzメソッドのテスト
+  def test_fizz_buzz
+    assert_equal "1", fizz_buzz(1)
+    assert_equal "2", fizz_buzz(2)
+    assert_equal "Fizz", fizz_buzz(3)
+    assert_equal "4", fizz_buzz(4)
+    assert_equal "Buzz", fizz_buzz(5)
+    assert_equal "Fizz", fizz_buzz(6)
+    assert_equal "Fizz Buzz", fizz_buzz(15)
+  end
+end
