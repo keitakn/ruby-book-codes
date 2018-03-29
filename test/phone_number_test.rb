fa536016d25e59fb76b26ad0ee96fc0b2ad60633
keitakn <keita.koga.work@gmail.com>
2018-03-29 22:27:15 +0900

    modify #8 少しだけ正規表現の精度を上げた

--- test/phone_number_test.rb
+++ test/phone_number_test.rb
@@ -13,5 +13,6 @@ class PhoneNumberTest < Minitest::Test
     assert_equal true, phone_number?("04992-1-2345")
     assert_equal true, phone_number?("03(1234)5678")
     assert_equal true, phone_number?("0795(12)3456")
+    assert_equal false, phone_number?("489-2121-4256")
   end
 end
