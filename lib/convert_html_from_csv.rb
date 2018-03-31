1f8d77b706660934fcd7f5c40a2f7ce7e548113a
keitakn <keita.koga.work@gmail.com>
2018-04-01 00:35:13 +0900

    add #19 HTML → CSV変換メソッドの仮実装を追加

---
+++ lib/convert_html_from_csv.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# HTML → CSV変換
+#
+# @param [String]
+# @return [String]
+def convert_html_from_csv(html)
+  <<-TEXT
+    wii_u,Wii U
+    ps4,プレステ4
+    gb,ゲームボーイ
+  TEXT
+end
---
+++ test/convert_html_from_csv_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "./lib/convert_html_from_csv"
+
+# HTML → CSV変換メソッドのテストクラス
+class ConvertHtmlFromCsvTest < Minitest::Test
+  # HTML → CSV変換メソッドのテスト
+  def test_convert_html_from_csv
+    html = <<-TEXT
+    <select name="game_console">
+    <option value="wii_u">Wii U</option>
+    <option value="ps4">プレステ4</option>
+    <option value="gb">ゲームボーイ</option>
+    </select>
+    TEXT
+
+    expected = <<-TEXT
+    wii_u,Wii U
+    ps4,プレステ4
+    gb,ゲームボーイ
+    TEXT
+
+    assert_equal expected, convert_html_from_csv(html)
+  end
+end
