13b2f216dc0c3599daa28fe8fbd08e55992d1410
keitakn <keita.koga.work@gmail.com>
2018-04-01 22:14:10 +0900

    modify #20 メソッド名をremove_particular_stringに変更（herokuに限らず利用シーンがある為）

--- lib/trim.rb
+++ lib/trim.rb
@@ -8,10 +8,10 @@ def remove_tab_and_space(text)
   text.gsub(/:\s*/, ": ")
 end
 
-# herokuのログから特定の文字列を抜き出す
+# 特定の文字列を取り除く
 #
 # @param [String]
 # @return [String]
-def heroku_log_replace(text)
+def remove_particular_string(text)
   text.gsub(/^.+heroku\/(api|scheduler).+\n/, "")
 end
