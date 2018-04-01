4a8b849572f72da1189e9ed21ea1263f3e3fa4f5
keitakn <keita.koga.work@gmail.com>
2018-04-01 20:14:38 +0900

    add #20 タブ、スペースを取り除くメソッドの仮実装を追加

---
+++ lib/trim.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# テキストからタブとスペースを取り除く
+#
+# @param [String]
+# @return [String]
+def remove_tab_and_space(text)
+  <<-TEXT
+{
+  japan: 'yen',
+  america: 'dollar',
+  italy: 'euro'
+}
+  TEXT
+end
---
+++ test/trim_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "./lib/trim"
+
+# テキスト整形のテストクラス
+class TrimTest < Minitest::Test
+  # タブ、スペースを取り除くメソッドのテスト
+  def test_remove_tab_and_space
+    text = <<-TEXT
+{
+  japan:	'yen',
+  america:'dollar',
+  italy:     'euro'
+}
+    TEXT
+
+    expected = <<-TEXT
+{
+  japan: 'yen',
+  america: 'dollar',
+  italy: 'euro'
+}
+    TEXT
+
+    assert_equal expected, remove_tab_and_space(text)
+  end
+end
