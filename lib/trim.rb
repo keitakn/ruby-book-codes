e1eaf62fa46c8c10fd3ea09198e50f58c7754341
keitakn <keita.koga.work@gmail.com>
2018-04-01 20:36:21 +0900

    add #20 herokuログから特定の文字列を取り出すメソッドの仮実装を追加

--- lib/trim.rb
+++ lib/trim.rb
@@ -7,3 +7,15 @@
 def remove_tab_and_space(text)
   text.gsub(/:\s*/, ": ")
 end
+
+# herokuのログから特定の文字列を抜き出す
+#
+# @param [String]
+# @return [String]
+def heroku_log_replace(text)
+  <<-TEXT
+Feb 14 07:36:46 heroku/api:  Starting process ...
+Feb 14 07:36:50 heroku/scheduler.7625:  Starting ...
+Feb 14 07:36:50 heroku/scheduler.7625:  State ...
+  TEXT
+end
