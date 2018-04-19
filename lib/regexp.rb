5c352e6460eeb38eeadbd0b1172dec4b74bda189
keitakn <keita.koga.work@gmail.com>
2018-04-19 23:45:33 +0900

    add #21 count_full_match_countメソッドの仮実装を追加

---
+++ lib/regexp.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# 完全一致した数を返す
+# @param text [String]
+# @param search_Word [String]
+# @return [Number]
+def count_full_match_count(text, search_Word)
+  2
+end
---
+++ test/regexp_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "./lib/regexp"
+
+# lib/regexp.rbのテストクラス
+class RegexpTest < Minitest::Test
+  # count_full_match_countメソッドのテスト
+  def test_count_full_match_count
+    text = <<-TEXT
+sounds that are pleasing to the ear.
+ear is the organ of the sense of hearing.
+I can't bear it.
+Why on earth would anyone feel sorry for you?
+    TEXT
+
+    assert_equal 2, count_full_match_count(text, "ear")
+  end
+end
