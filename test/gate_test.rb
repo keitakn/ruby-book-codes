76fc0f733007f2f7d0872dfc76938af504afcc0b
keitakn <keita.koga.work@gmail.com>
2018-05-01 00:38:34 +0900

    add #9 各駅のインスタンスをsetupメソッド内で作るようにリファクタリング

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -6,27 +6,24 @@ require "./lib/ticket"
 
 # 改札機プログラムのテスト
 class GateTest < Minitest::Test
+  # テストメソッド実行前にこのメソッドが毎回呼ばれる
+  def setup
+    @harajuku = Gate.new(:harajuku)
+    @shibuya = Gate.new(:shibuya)
+    @ebisu = Gate.new(:ebisu)
+  end
+
   # 原宿 → 渋谷の移動が出来る事（trueが返ってくる）を確認する
   def test_harajuku_to_shibuya
-    harajuku = Gate.new(:harajuku)
-    shibuya = Gate.new(:shibuya)
-
     ticket = Ticket.new(150)
-
-    harajuku.enter(ticket)
-
-    assert shibuya.exit(ticket)
+    @harajuku.enter(ticket)
+    assert @shibuya.exit(ticket)
   end
 
   # 原宿 → 恵比寿の移動が出来ない事（falseが返ってくる）を確認する
   def test_harajuku_to_ebisu_when_fare_is_not_enough
     ticket = Ticket.new(150)
-
-    harajuku = Gate.new(:harajuku)
-    ebisu = Gate.new(:ebisu)
-
-    harajuku.enter(ticket)
-
-    refute ebisu.exit(ticket)
+    @harajuku.enter(ticket)
+    refute @ebisu.exit(ticket)
   end
 end
