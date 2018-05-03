b6496c442914c61902628798fec9b1a2b3f84f43
keitakn <keita.koga.work@gmail.com>
2018-05-03 22:58:48 +0900

    fix #12 行間を調整

--- test/effects_test.rb
+++ test/effects_test.rb
@@ -8,25 +8,21 @@ class EffectsTest < Minitest::Test
   # 単語を逆順にする効果のテスト
   def test_reverse
     effect = Effects.reverse
-
     assert_equal "ybuR si !nuf", effect.call("Ruby is fun!")
   end
 
   # エコー効果のテスト
   def test_echo
     effect = Effects.echo(2)
-
     assert_equal "RRuubbyy iiss ffuunn!!", effect.call("Ruby is fun!")
   end
 
   # ラウド効果のテスト
   def test_loud
     effect = Effects.loud(2)
-
     assert_equal "RUBY!! IS!! FUN!!!", effect.call("Ruby is fun!")
 
     effect = Effects.loud(3)
-
     assert_equal "RUBY!!! IS!!! FUN!!!!", effect.call("Ruby is fun!")
   end
 end
