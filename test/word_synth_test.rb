f4d2e2be8667918a78c0e1cccaabf47aae48de32
keitakn <keita.koga.work@gmail.com>
2018-05-03 23:15:39 +0900

    add #12 復数の効果を与えて実行するテストを追加

--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -18,4 +18,13 @@ class WordSynthTest < Minitest::Test
     synth.add_effect(Effects.reverse)
     assert_equal "ybuR si !nuf", synth.play("Ruby is fun!")
   end
+
+  # 復数の効果を与えて実行するテスト
+  def test_play_with_many_effects
+    synth = WordSynth.new
+    synth.add_effect(Effects.echo(2))
+    synth.add_effect(Effects.loud(3))
+    synth.add_effect(Effects.reverse)
+    assert_equal "!!!YYBBUURR !!!SSII !!!!!NNUUFF", synth.play("Ruby is fun!")
+  end
 end
