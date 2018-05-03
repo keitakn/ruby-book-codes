e8a969507d1b10d244274295f620b6cce9302fd1
keitakn <keita.koga.work@gmail.com>
2018-05-03 22:40:07 +0900

    add #12 reverseメソッドの仮実装を追加

--- lib/effects.rb
+++ lib/effects.rb
@@ -3,6 +3,11 @@
 # 単語に与える効果メソッドを定義する
 module Effects
   # 単語を逆順にする効果メソッド
+  #
+  # @return [Proc]
   def self.reverse
+    ->(words) do
+      words.split(" ").map(&:reverse).join(" ")
+    end
   end
 end
--- test/effects_test.rb
+++ test/effects_test.rb
@@ -7,6 +7,8 @@ require "./lib/effects"
 class EffectsTest < Minitest::Test
   # 単語を逆順にする効果のテスト
   def test_reverse
-    assert Effects
+    effect = Effects.reverse
+
+    assert_equal "ybuR si !nuf", effect.call("Ruby is fun!")
   end
 end
